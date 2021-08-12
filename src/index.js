7834a9a8e14413458a5d3f59e72ac2d5ae654de3
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-12 16:25:55 -0400

    useState1 imported

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+// import State from './components/useState';
+import UseState1 from './components/useState1';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      Hello
+      {/* passing initialValue as */}
+      {/* <State initialValue={0} /> */}
+      <UseState1 />
+    </div>
+  );
 };
 
 ReactDom.render(<App />, document.getElementById('root'));
