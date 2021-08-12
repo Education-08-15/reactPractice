ae448d9cc056ca0bb5277ee2dbeb78ac5311498d
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-12 19:00:43 -0400

    useEffect file imported

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Effect from './components/useEffect';
 import State from './components/useState';
 import UseState1 from './components/useState1';
 
@@ -8,8 +9,9 @@ const App = () => {
     <div>
       Hello
       {/* passing initialValue as prop */}
-      <State initialValue={0} />
-      <UseState1></UseState1>
+      {/* <State initialValue={0} /> */}
+      {/* <UseState1></UseState1> */}
+      <Effect />
     </div>
   );
 };
